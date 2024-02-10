06be9e5c8c88399f2392954e24716f2bcd102a6d
Mauri <mauri.monzon91@gmail.com>
2024-02-10 17:36:25 -0300

    Creando y Pidiendo todos los proyectos

--- src/controllers/projects.Controller.js
+++ src/controllers/projects.Controller.js
@@ -1,7 +1,17 @@
-export const getProjects = (req, res) => {
-  res.send("Get projects");
+import { Project } from "../models/Project.js";
+
+export const getProjects = async (req, res) => {
+  const projects = await Project.findAll();
+  res.json(projects);
 };
 
-export const createProject = (req, res) => {
-  res.send("Creating projects");
+export const createProject = async (req, res) => {
+  const { name, priority, description } = req.body;
+  const newProject = await Project.create({
+    name,
+    priority,
+    description,
+  });
+
+  res.json(newProject);
 };
