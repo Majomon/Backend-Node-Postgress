583e10a6c90d8683fc93a90faf6a7eb5f4209a6c
Mauri <mauri.monzon91@gmail.com>
2024-02-10 17:39:21 -0300

    Trycatch para manejar errores en GetAll y PostProject

--- src/controllers/projects.Controller.js
+++ src/controllers/projects.Controller.js
@@ -1,17 +1,26 @@
 import { Project } from "../models/Project.js";
 
 export const getProjects = async (req, res) => {
-  const projects = await Project.findAll();
-  res.json(projects);
+  try {
+    const projects = await Project.findAll();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const createProject = async (req, res) => {
   const { name, priority, description } = req.body;
-  const newProject = await Project.create({
-    name,
-    priority,
-    description,
-  });
 
-  res.json(newProject);
+  try {
+    const newProject = await Project.create({
+      name,
+      priority,
+      description,
+    });
+
+    res.json(newProject);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
