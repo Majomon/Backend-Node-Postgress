f0ed73168ba92f21f51cb09355989865db6cf6d8
Mauri <mauri.monzon91@gmail.com>
2024-02-09 23:20:23 -0300

    act

--- src/database/database.js
+++ src/database/database.js
@@ -1,1 +1,6 @@
-import Sequelize from "sequelize"
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,{
+    
+}
+);
