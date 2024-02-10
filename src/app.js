8820649e0f7abe77c3da3ea21610f604a38b4574
Mauri <mauri.monzon91@gmail.com>
2024-02-09 23:05:59 -0300

    Volviendo a configurar

--- src/app.js
+++ src/app.js
@@ -1,16 +1,6 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+/* require("dotenv").config(); */
+import express from "express";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend con NodeJS - Express - CRUD Api Rest ");
-});
-
-app.listen(port, () => console.log(`Server running on ${port}`));
+export default app;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+/* Para poder usar import y export debo crear en el package "type": "module" */
+import app from "./app.js";
+
+app.listen(3000);
